2ec581c91a33015c0fbd1768b27dd6e93a66895d
Vadim <gabderahmanov99@gmail.com>
2020-09-12 20:38:19 +0400

    first commit

--- app/src/main/java/com/vadim/newsapi/MainActivity.java
+++ app/src/main/java/com/vadim/newsapi/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Gson gson = new GsonBuilder().create();
+
+    private final String URL = "http://newsapi.org/v2/top-headlines?" +
+            "country=us&" +
+            "apiKey=514fbafd2e864aa99c6c034edfee8799";
+
+    Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(gson)).baseUrl(URL).build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
